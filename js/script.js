785150dd3d337dbd13ccfe01cb09fdfd8c62899b
Kelsey Cox <kelseycox@Lucy.local>
2019-06-02 22:58:45 -0700

    finished my game

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-// variable declarations
 var text = '';
 var arr1 = [];
 var inputString = '';
 var correctArr = [];
 var count = 7;
+var parts = ['rightleg', 'leftleg', 'rightarm', 'leftarm', 'body', 'head']
 
-
-//HTML element references
 var inputEl = document.querySelector('[name="input"]');
 var textEl = document.querySelector('[name="start"]');
 var pEl = document.getElementById("usedletters");
@@ -14,58 +12,59 @@ var wordEl = document.getElementById("word");
 var btnEl = document.getElementById("submit");
 var startEl = document.getElementById("start");
 
-//functions and stuff
 
 function startGame() {
-    //set text to the inputText from the game
     text = textEl.value;
     correctArr = [];
     arr1 = text.split('');
     arr1.forEach(function(letter) {
         correctArr.push('_');
     });
-    console.log(correctArr);
-    //display carrectArr in box
     wordEl.textContent = correctArr.join('   ');
-    //set count to 7
-    count = 7;
-    //set inputEl.textContent to an empty array
+    count = 7; 
+    parts.forEach(function (part) {
+        document.getElementById(part).style.border = "transparent";
+        document.getElementById(part).style.color = "transparent";
+    });
     textEl.value = '';
     pEl.value = [];
+    document.getElementById("sadface").innerHTML = ":(";
 }
 
-//var correctness
 function correctness(str) {
-        if (arr1.includes(str)) {    
-            for (var i = 0; i < arr1.length; i++) {
-                if (arr1[i] === str) {
-                correctArr[i] = str;
-                wordEl.textContent =  correctArr.join('   ');
-                }
+    if (arr1.includes(str)) {    
+        for (var i = 0; i < arr1.length; i++) {
+            if (arr1[i] === str) {
+            correctArr[i] = str;
+            wordEl.textContent =  correctArr.join('   ');
             }
+        }
+    } else {
+        count = count - 1;
+        if (count === 0) {
+            document.getElementById("sadface").style.color = "black"
+            wordEl.textContent = "You Lose!";
         } else {
-            count = count - 1;
-            if (count === 0) {
-                console.log("you lose")
-            }
-            inputString = inputString + "    " + str;
-            pEl.textContent = inputString;
-        };
+        document.getElementById(parts[count - 1]).style.border = "1px solid black";
+        inputString = inputString + "    " + str;
+        pEl.textContent = inputString;
+        }
+    };
 };
 
-function hangman() {
-    //if !correctArr.includes('_'), you win!
-    //or else, if count reaches 0 you lose!
-    // for pictures, access based on count?
-}
-//create all event listeners (most stuff will be here)
 btnEl.addEventListener("click", function(t) {    
     var textInput = inputEl.value;
     correctness(textInput)
     inputEl.value = '';
+    if (correctArr.join('') === text) {
+        parts.forEach(function(part) {
+            document.getElementById(part).style.border = "1px solid black";
+        });
+        document.getElementById("noose").style.border = "transparent";
+        document.getElementById("sadface").innerHTML = ":D";
+        document.getElementById("sadface").style.color = "black";
+        pEl.textContent = "You Win!";
+    }
 });
 
 startEl.addEventListener("click", startGame);
-//any additional function to make it work
-
-// addEventListener("")
